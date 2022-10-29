4532a9b21098ffed17283299d2ae2dd1986c44f3
Pedro Ricardo Oliveira Córdova <pedroricardocordova@yahoo.com.br>
2022-10-28 21:13:53 -0300

    fix: updated title

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -3,7 +3,7 @@ import Main from '../template/Main'
 
 export default props =>
   <Main icon="home" title="Inicio"
-    subtitle="Segundo projeto do capitulo de react.">
+    subtitle="Sistema de cadastro de usuários.">
     <div className="display-4">Bem Vindo!</div>
     <hr />
     <p className="mb-0">Sistema de cadastro de usuário.</p>
