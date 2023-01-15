1477387942a0878dbf887279bade1b935ff3d8ba
Pedro Ricardo Oliveira Córdova <pedroricardocordova@yahoo.com.br>
2023-01-14 21:04:31 -0300

    fea : end stylish

---
+++ frontend/src/components/user/userCrud.test.js
@@ -0,0 +1,9 @@
+const userCrud = require('../userCrud')
+
+describe('Testes do Crud', () => {
+
+    it('should add a new user', () => {
+        expect(userCrud.save()
+    })
+
+})
--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { BrowserRouter } from 'react-router-dom'
-import 'font-awesome/css/font-awesome.min.css'
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
+import "font-awesome/css/font-awesome.min.css";
 
-import Logo from '../components/template/Logo'
-import Nav from '../components/template/Nav'
+import Logo from "../components/template/Logo";
+import Nav from "../components/template/Nav";
 
-import Routes from './routes'
+import Routes from "./routes";
 
+import Footer from "../components/template/Footer";
 
-import Footer from '../components/template/Footer'
-
-export default props =>
+export default (props) => (
   <BrowserRouter>
-
     <div className="app">
       <Logo />
       <Nav />
       <Routes />
       <Footer />
-    </div >
+    </div>
   </BrowserRouter>
+);
